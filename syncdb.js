accf0b016dc4e30e1c3381041a9adff946f06607
mimic_vr <142630565+Hamm993@users.noreply.github.com>
2025-01-18 15:56:49 -0500

    Update syncdb.js

--- syncdb.js
+++ syncdb.js
@@ -2,4 +2,4 @@ const Guild = require('./models/guild')
 const Infraction = require('./models/infraction')
 
 // Guild.sync({alter: true});
-Infraction.sync({force: true});
+// Infraction.sync({force: true});
